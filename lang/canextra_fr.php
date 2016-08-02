04036ac2325955fb7649d2433602774333bbc968
Debondt Didier <p@henix.be>
2016-08-02 12:57:19 +0200

    Encore un peu de psr

--- lang/canextra_fr.php
+++ lang/canextra_fr.php
@@ -1,12 +1,12 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
 	'canextra_titre' => 'canvas_extra',
 );
-
-?>
